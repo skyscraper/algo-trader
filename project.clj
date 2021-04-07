70c88ee5e6234ef6a3994730ec2acc891dfc40fc
Daniel Lee <dan@oak.local>
2021-04-06 19:40:21 -0500

    bump clj

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :dependencies [[aleph "0.4.7-alpha7"]
                  [cheshire "5.10.0"]
                  [com.datadoghq/java-dogstatsd-client "2.11.0"]
-                 [org.clojure/clojure "1.10.2"]
+                 [org.clojure/clojure "1.10.3"]
                  [org.clojure/core.async "1.3.610"]
                  [org.clojure/tools.logging "1.1.0"]]
   :repl-options {:init-ns algo-trader.core}
