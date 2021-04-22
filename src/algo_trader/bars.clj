2e8897ace70b23a01c86e4fdc138b94207bfd5a4
Daniel Lee <dan@oak.local>
2021-04-22 12:30:56 -0500

    use close again

--- src/algo_trader/bars.clj
+++ src/algo_trader/bars.clj
@@ -13,20 +13,20 @@
 (defn update-bar [bar {:keys [price size side]}]
   (let [amt (* price size)
         op (if (= :buy side) + -)]
-    (-> (update bar :amt + amt)
+    (-> (assoc bar :c price)
+        (update :amt + amt)
         (update :v + size)
         (update :imb op amt))))
 
 (defn add-to-bars
   [{:keys [current bars ewms target-amt] :as acc}
-   trade]
-  (let [{:keys [amt v] :as updated} (update-bar current trade)]
+   {:keys [price] :as trade}]
+  (let [{:keys [amt] :as updated} (update-bar current trade)]
     (if (>= amt target-amt)
-      (let [vwap1 (/ amt v)
-            {:keys [vwap] :or {vwap vwap1}} (last bars)
-            new-ewms (map #(ewm-step %1 vwap1 %2) ewms window-alphas)
+      (let [{:keys [c] :or {c price}} (last bars)
+            new-ewms (map #(ewm-step %1 price %2) ewms window-alphas)
             new-ewmacs (map - new-ewms (drop fc-window-delta new-ewms))
-            new-bar (assoc updated :vwap vwap1 :diff (- vwap1 vwap))]
+            new-bar (assoc updated :diff (- price c))]
         (assoc
          (update acc :bars conj new-bar)
          :current base
